b9b4fe08b35aa071c17ca3e9901d498034228f27
Arun-projectdumps <ranaarun7805@gmail.com>
2026-08-13 01:50:48 +0530

    Time: 0 ms (100%), Space: 18.3 MB (27%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    static bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second;
+    }
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+
+        for (int x : nums) {
+            freq[x]++;
+        }
+
+        vector<pair<int, int>> v(freq.begin(), freq.end());
+
+        sort(v.begin(), v.end(), cmp);
+
+        vector<int> ans;
+
+        for (int i = 0; i < k; i++) {
+            ans.push_back(v[i].first);
+        }
+
+        return ans;
+    }
+};
