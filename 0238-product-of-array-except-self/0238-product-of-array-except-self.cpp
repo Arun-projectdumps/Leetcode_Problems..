26b91ea8df8264d30764d806aa7ed0bef783474d
Arun-projectdumps <ranaarun7805@gmail.com>
2026-08-17 16:27:11 +0530

    Time: 3 ms (37.16%), Space: 43.7 MB (5.05%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> pre;
+        pre.push_back(nums[0]);
+        vector<int>post (nums.size(),0);
+        post[nums.size()-1] = nums[nums.size()-1];
+        vector<int>ans (nums.size());
+        //finding out all the prefixes 
+        for(int x = 1; x < nums.size(); x++){
+            pre.push_back(nums[x]*pre[x-1]);
+        }
+        //finding all the postfixes
+        for(int x = nums.size()-2; x >=0 ;x--){
+            post[x] = (nums[x]*post[x+1]);
+        }
+        //settling the edges cases of start and end 
+        ans[0] = post[1];
+        ans[nums.size()-1] = pre[nums.size()-2];
+        //
+        for(int i = 1 ; i < nums.size()-1;i++){
+            ans[i] = pre[i-1]*post[i+1];
+        }
+        return ans;
+    }
+};
