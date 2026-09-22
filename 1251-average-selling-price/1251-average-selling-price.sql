283ed5eec84716bdbf1ca3cacde6d094eed9df69
Arun-projectdumps <ranaarun7805@gmail.com>
2026-09-22 22:53:58 +0530

    Time: 855 ms (71.8%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -4,6 +4,5 @@ SELECT
 FROM prices p 
 LEFT JOIN unitssold u 
     ON p.product_id = u.product_id 
-    and u.purchase_date >= p.start_date 
-    AND u.purchase_date <= p.end_date 
+    and u.purchase_date between p.start_date and p.end_date
 GROUP BY p.product_id;
